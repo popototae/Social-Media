b0fa5bd6346c9ca79daea9b11f0a508979c7d5c7
popototae <popototae@gmail.com>
2025-12-04 21:39:39 +0700

    initial backend

---
+++ server/server.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('./User');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error('DB Connection Error:', err));
+
+app.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const existingUser = await User.findOne({ $or: [{ email }, { username }] })
+        if (existingUser) {
+            return res.status(400).json({ message: 'User or Email already exists' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, await bcrypt.genSalt(10));
+
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+        res.status(201).json({ message: 'User registered successfully!' });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+});
+
+
+app.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign(
+            { id: user._id, username: user.username },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+        ); 
+
+        res.json({
+            message: 'Login successful',
+            token,
+            user: { id: user._id, username: user.username, email: user.email }
+        });
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
